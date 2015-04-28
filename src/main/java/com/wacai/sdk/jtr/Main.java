316c44293c07d5a8fdeb381a0612d9e6384acca0
zhongl <zhong.lunfu@gmail.com>
2015-04-28 20:52:34 +0800

    Fixed tld not found.
         org.apache.jasper.JasperException: The absolute uri: http://java.sun.com/jsp/jstl/core cannot be resolved in either web.xml or the jar files deployed with this application

--- src/main/java/com/wacai/sdk/jtr/Main.java
+++ src/main/java/com/wacai/sdk/jtr/Main.java
@@ -1,7 +1,5 @@
 package com.wacai.sdk.jtr;
 
-import org.apache.tomcat.InstanceManager;
-import org.apache.tomcat.SimpleInstanceManager;
 import org.eclipse.jetty.annotations.ServletContainerInitializersStarter;
 import org.eclipse.jetty.apache.jsp.JettyJasperInitializer;
 import org.eclipse.jetty.plus.annotation.ContainerInitializer;
@@ -10,6 +8,7 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.ContextHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.resource.JarResource;
 import org.eclipse.jetty.webapp.WebAppContext;
 
 import java.io.File;
@@ -55,13 +54,25 @@ public final class Main {
     }
 
     ContextHandler jsp(String ctx, File templateDir, String descriptor) {
+        File tempDir = new File(System.getProperty("java.io.tmpdir"));
+        File scratchDir = new File(tempDir.toString(), "jtr");
+        if (!scratchDir.exists()) {
+            if (!scratchDir.mkdirs()) {
+                throw new RuntimeException("Unable to create scratch directory: " + scratchDir);
+            }
+        }
         final WebAppContext context = new WebAppContext(templateDir.getAbsolutePath(), ctx);
         context.setDescriptor(descriptor);
+
+        final URL url = getClass().getProtectionDomain().getCodeSource().getLocation();
+        if (url != null) {
+            // tld could be scanned under the META-INF/ after add shade jar
+            context.getMetaData().addWebInfJar(JarResource.newResource(url));
+        }
         context.setAttribute(INCLUDE_JAR_PATTERN, JSP_PATTERN);
-        context.setAttribute("javax.servlet.context.tempdir", "/tmp/jsp");
+        context.setAttribute("javax.servlet.context.tempdir", scratchDir);
         context.setAttribute("org.eclipse.jetty.containerInitializers", jspInitializers());
         context.addBean(new ServletContainerInitializersStarter(context), true);
-        context.setAttribute(InstanceManager.class.getName(), new SimpleInstanceManager());
         context.setClassLoader(new URLClassLoader(new URL[0], getClass().getClassLoader()));
         return context;
     }
