ee13459018f4890f097b794c8a7b427aeada879a
zhongl <zhong.lunfu@gmail.com>
2015-04-29 11:05:34 +0800

    Fixed #4:Ignore .json file missing.

--- src/main/java/com/wacai/sdk/jtr/InjectModelServlet.java
+++ src/main/java/com/wacai/sdk/jtr/InjectModelServlet.java
@@ -1,6 +1,8 @@
 package com.wacai.sdk.jtr;
 
 import com.alibaba.fastjson.JSONReader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -16,23 +18,31 @@ public class InjectModelServlet extends HttpServlet {
 
     private final File   jsonDir;
     private final String jspContextPath;
+    private final Logger logger;
 
     public InjectModelServlet(File jsonDir, String jspContextPath) {
         this.jsonDir = jsonDir;
         this.jspContextPath = jspContextPath;
+        logger = LoggerFactory.getLogger(getClass());
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         final String path = req.getServletPath();
         String name = get(req.getQueryString(), "m", base(path));
-        setAttributes(req, new JSONReader(new FileReader(new File(jsonDir, name + ".json"))));
+        final File file = new File(jsonDir, name + ".json");
+        if (file.exists()) {
+            setAttributes(req, new JSONReader(new FileReader(file)));
+        } else {
+            logger.warn("Missing model file {}", file);
+        }
         req.getServletContext().getContext(jspContextPath).getRequestDispatcher(path).forward(req, resp);
     }
 
     @SuppressWarnings({"unchecked"})
     private void setAttributes(HttpServletRequest req, JSONReader reader) {
         final Map<String, String> model = (Map<String, String>) reader.readObject(new HashMap<String, String>());
+        logger.info("Inject model {}", model);
         try {
             for (String key : model.keySet()) {
                 req.setAttribute(key, model.get(key));
@@ -47,7 +57,7 @@ public class InjectModelServlet extends HttpServlet {
     }
 
     static String get(String queryString, String key, String defaultValue) {
-        if(queryString == null) return defaultValue;
+        if (queryString == null) return defaultValue;
         final int i = queryString.indexOf(key);
         if (i < 0) return defaultValue;
         final int j = queryString.indexOf('&', i);
