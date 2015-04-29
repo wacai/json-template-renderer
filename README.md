083f6764747de07486a5ef48d3eaab86e93a43b2
zhongl <zhong.lunfu@gmail.com>
2015-04-29 14:09:33 +0800

    Fixed help info.
    Clean code.
    
    Update README.

--- README.md
+++ README.md
@@ -4,15 +4,35 @@
 git clone git@github.com:wacai/json-template-renderer.git
 cd json-template-renderer
 mvn clean package
-java -jar target/jtr-0.0.1-SNAPSHOT.jar src/test/resources/webapp src/test/resources/models
+java -jar target/jtr-*.jar src/test/resources/webapp src/test/resources/models
 ```
 
 1. Open <http://localhost:8080>
 1. Click [home.jsp](http://localhost:8080/home.jsp)
 
+## Template Supported List
+
+* [x] [Java Server Pages][jsp] ([JSP Standard Tag Library][jstl])
+* [ ] [Velocity][vm]
+* [ ] [FreeMarker][ftl]
+
+[jsp]:http://www.oracle.com/technetwork/java/jsp-138432.html
+[jstl]:http://www.oracle.com/technetwork/java/index-jsp-135995.html
+[vm]:http://velocity.apache.org/
+[ftl]:http://freemarker.org/
 
 ## More help
 
 ```
-java -jar target/jtr-0.0.1-SNAPSHOT.jar help
+java -jar target/jtr-*.jar help
 ```
+
+## Development Tips
+
+### Run & Debug
+
+```
+mvn clean compile exec:exec
+```
+
+Use your favourite IDE setup remote debug to 5005
--- src/main/java/com/wacai/sdk/jtr/Main.java
+++ src/main/java/com/wacai/sdk/jtr/Main.java
@@ -8,7 +8,6 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.ContextHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import org.eclipse.jetty.util.resource.JarResource;
 import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.webapp.WebAppContext;
 
@@ -30,7 +29,7 @@ public final class Main {
     public static void main(String[] args) throws Exception {
         if (args.length > 0 && args.length != 2) {
             System.err.println("Arguments  : <template-dir> <json-dir>");
-            System.err.println("Properties : -Dserver.port=8080 -Dcontext.descriptor=testweb.xml");
+            System.err.println("Properties : -Dserver.port=8080 -Dcontext.descriptor=/path/to/web.xml");
             System.exit(1);
         }
 
@@ -47,7 +46,7 @@ public final class Main {
         server.join();
     }
 
-    public Handler handler(File templateDir, File jsonDir, String descriptor) throws Exception {
+    Handler handler(File templateDir, File jsonDir, String descriptor) throws Exception {
         final Route route = new Route(templateDir.getAbsolutePath());
         injectModel("/jsp", templateDir, jsonDir, descriptor, route);
         return route.asHandler();
@@ -86,14 +85,13 @@ public final class Main {
         route.register(jsp(jspContextPath, templateDir, descriptor));
     }
 
-    private static List<ContainerInitializer> jspInitializers() {
+    static List<ContainerInitializer> jspInitializers() {
         JettyJasperInitializer sci = new JettyJasperInitializer();
         ContainerInitializer initializer = new ContainerInitializer(sci, null);
         return Collections.singletonList(initializer);
     }
 
-
-    private static File getDir(String[] args, int index, String defaultValue) {
+    static File getDir(String[] args, int index, String defaultValue) {
         final File file = args.length == 0 ? new File(defaultValue) : new File(args[index]);
         if (!file.exists() || !file.isDirectory()) throw new IllegalArgumentException(file + " is not a existed dir.");
         return file;
