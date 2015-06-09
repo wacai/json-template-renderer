e50bc21355f38935728b77dec124f848e8c5ecaf
zhongl <zhong.lunfu@gmail.com>
2015-06-09 14:24:02 +0800

    Fixed #11: Support non-get http method.

--- src/main/java/com/wacai/sdk/jtr/Dispatch.java
+++ src/main/java/com/wacai/sdk/jtr/Dispatch.java
@@ -1,5 +1,6 @@
 package com.wacai.sdk.jtr;
 
+import org.eclipse.jetty.http.HttpMethod;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 
@@ -23,6 +24,8 @@ public class Dispatch extends ResourceHandler {
     public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         final String path = path(RequestPath.suffix(target));
         if (!paths.contains(path)) {
+            if (!"GET".equals(baseRequest.getMethod()))
+                baseRequest.setMethod(HttpMethod.GET, HttpMethod.GET.toString());
             super.handle(target, baseRequest, request, allowCrossOrigin(response));
         } else {
             baseRequest.getContext().getContext(path).getRequestDispatcher(target).forward(request, allowCrossOrigin(response));
@@ -34,8 +37,8 @@ public class Dispatch extends ResourceHandler {
     }
 
     static HttpServletResponse allowCrossOrigin(HttpServletResponse response) {
-        response.setHeader("Access-Control-Allow-Origin","*");
-        response.setHeader("Access-Control-Allow-Methods","GET, OPTIONS");
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
         return response;
     }
 
